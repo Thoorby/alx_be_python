897d16f3db5020e2b2bf28591b3285453e61863f
oluwatobi <ojothrby@gmail.com>
2025-06-08 14:50:11 +0200

    fix: use exact expression for CELSIUS_TO_FAHRENHEIT_FACTOR as 9/5 for compliance

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,9 +2,8 @@
 # Description: Temperature conversion tool using global conversion factors
 
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius using the global conversion factor."""
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
